c922a6242ed8ad9da4c89e02b12fcce0831f9e80
Sourabh choudhary <sourabhskc30@gmail.com>
2020-05-26 16:51:53 +0530

    code canged

--- tweek.py
+++ tweek.py
@@ -1,8 +1,8 @@
 import os
-accuracy = os.system("cat /env_code/accuracy.txt")
+accuracy = os.system("cat /dsp/accuracy.txt")
 x = 'model.add(Dense(units=64, activation=\"relu\"))'
 if accuracy < 85:
-    os.system("sed -i '/sigmoid/ i {}' /env_code/train.py".format(x))
+    os.system("sed -i '/sigmoid/ i {}' /dsp/train.py".format(x))
 else:
     print("Good going :)")
     exit()
