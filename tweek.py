3332d1ff4c7f3650a970de8ed9c6ec957262829d
Sourabh choudhary <sourabhskc30@gmail.com>
2020-05-26 14:02:52 +0530

    Job3

--- tweek.py
+++ tweek.py
@@ -1,8 +1,8 @@
-import os
-accuracy = os.system("cat /client/accuracy.txt")
+import os
+accuracy = os.system("cat /env_code/accuracy.txt")
 x = 'model.add(Dense(units=64, activation=\"relu\"))'
 if accuracy < 85:
-    os.system("sed -i '/sigmoid/ i {}' /client/client.py".format(x))
+    os.system("sed -i '/sigmoid/ i {}' /env_code/train.py".format(x))
 else:
     print("Good going :)")
     exit()
