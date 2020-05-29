064d0b9a8833213a6974a83998373e58862db62a
sourabhskc <64469149+sourabhskc@users.noreply.github.com>
2020-05-29 18:20:02 +0530

    Delete tweek.py

--- tweek.py
+++
@@ -1,8 +0,0 @@
-import os
-accuracy = os.system("cat /dsp/accuracy.txt")
-x = 'model.add(Dense(units=64, activation=\"relu\"))'
-if accuracy < 85:
-    os.system("sed -i '/sigmoid/ i {}' /dsp/train.py".format(x))
-else:
-    print("Good going :)")
-    exit()
