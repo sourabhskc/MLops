132b8103bfe0616342bd52aa21a0386ba0ab0260
sourabhskc <64469149+sourabhskc@users.noreply.github.com>
2020-05-31 18:51:00 +0530

    accuracy changed to 80

--- tweak.py
+++ tweak.py
@@ -1,8 +1,8 @@
 import os
 accuracy = os.system("cat /taskCode/accuracy.txt")
 x = 'model.add(Dense(units=32, activation=\"relu\"))'
-if accuracy < 85:
+if accuracy < 80:
     os.system("sed -i '/sigmoid/ i {}' /taskCode/train.py".format(x))
 else:
-    print("Good going :)")
+    print("You Achieved wnated accuracy :)")
     exit()
